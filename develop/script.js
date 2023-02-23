a602cafb0468e613adc0a93a14ed501e25f47f44
Pilotguide9897 <120068344+Pilotguide9897@users.noreply.github.com>
2023-02-23 12:21:19 -0400

    Update script.js
    Added extra api keys.

--- develop/script.js
+++ develop/script.js
@@ -1,2 +1,13 @@
 var tmdbAPIKey = '60284bb58aafe269068499987d0a2596';
 var watchmodeAPIKey = 'LoVEu2tw5mIYG5E37IhSybc6HmM2ovxVxxx8VJxf'; 
+var omdbAPIKey = '83368b28'; //http://www.omdbapi.com/?i=tt3896198&apikey=83368b28
+var imdbAPIKey = 'k_aw3dylfc'; 
+
+//API Fetch Template
+fetch(---)
+.then((response) => {
+    response.json();
+    if (!response.ok) {
+      throw new Error(`HTTP error!`);
+    }
+ .then((data) => console.log(data));
