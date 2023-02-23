467333af70504a4e2a119521651a5a9e556413b8
Pilotguide9897 <120068344+Pilotguide9897@users.noreply.github.com>
2023-02-23 10:20:34 -0400

    Update script.js

--- develop/script.js
+++ develop/script.js
@@ -1,2 +1,2 @@
-var tmdbAPIKey = 60284bb58aafe269068499987d0a2596;
-var watchmodeAPIKey = LoVEu2tw5mIYG5E37IhSybc6HmM2ovxVxxx8VJxf; 
+var tmdbAPIKey = '60284bb58aafe269068499987d0a2596';
+var watchmodeAPIKey = 'LoVEu2tw5mIYG5E37IhSybc6HmM2ovxVxxx8VJxf'; 
