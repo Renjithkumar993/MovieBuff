2a5b52dd5a39f8db20b743df90c6398da53648bb
Justin <justinconnors@hotmail.ca>
2023-02-23 21:13:44 -0400

    user can search for a movie with a prompt(will change to search box) and an array of movies matching the keyword(s) displays in the console

--- develop/tmdb.js
+++ develop/tmdb.js
@@ -1,15 +1,27 @@
 // function that fetches the TMDb API
-function TMDb_() {
+async function TMDb_(searchQuery) {
     //TMDb api Key
     const TMDb_KEY = '60284bb58aafe269068499987d0a2596';
-    //Initializing search parameters
-    var search = '';
+    //API url call
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDb_KEY}&query=${searchQuery}`;
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDb_KEY}&query=${search}`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+// call the function that takes in the search, using prompt for now, will change to search box
+const searchQueryInput = prompt("enter a movie title to search for:");
+
+TMDb_(searchQueryInput)
+    .then((movies) => {
+        console.log(movies);
+// Do something with the movies array
+})
+    .catch((error) => {
+        console.error(error);
+});
