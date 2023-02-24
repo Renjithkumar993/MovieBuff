625a1f7523b5a0f846d982c77af57d97c1df398a
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-02-24 10:47:55 -0400

    added element ids

--- develop/wm.js
+++ develop/wm.js
@@ -1,16 +1,21 @@
+// Section for selecting HTML elements from the DOM and assigning them to JavaScript variables.
+const movieInputEl = document.getElementById("movieName");
+const submitButtonEl = document.getElementById("searchBtn")
+
+// Initializing search parameters
+var searchQuery = movieInputEl.value;
+
+// Adding Event Listener to 
+movieInputEl.addEventListener('click', watchMode());
+
 // Function that fetches the Watchmode API data
 function watchMode(searchQuery) { 
 
 //Watchmode API key
 const WMKey = 'LoVEu2tw5mIYG5E37IhSybc6HmM2ovxVxxx8VJxf';
 
-var submitMovieTitle = document.getElementById("name");
-// Initializing search parameters
-var movieTitle = submitMovieTitle.value;
-
-
 // Search for the movie using the Watchmode API's search functionality
-fetch(`https://api.watchmode.com/v1/search/?apiKey=${WMKey}&type=movie&search_field=name&search_value=${encodeURIComponent(movieTitle)}`)
+fetch(`https://api.watchmode.com/v1/search/?apiKey=${WMKey}&type=movie&search_field=name&search_value=${encodeURIComponent(searchQuery)}`)
   .then(function(response) {
     return response.json()
   })
