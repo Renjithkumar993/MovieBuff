c8747917feaf3ed8bf029dcec3d569af40604a29
Justin <justinconnors@hotmail.ca>
2023-02-24 15:36:41 -0400

    able to display movie info from api array on 1 card working on displaying all next

--- develop/tmdb.js
+++ develop/tmdb.js
@@ -20,7 +20,20 @@ const searchQueryInput = prompt("enter a movie title to search for:");
 TMDb_(searchQueryInput)
     .then((movies) => {
         console.log(movies);
-// Do something with the movies array
+        for (let i = 0; i < movies.length; i++) {
+            var posterPath = `https://image.tmdb.org/t/p/original${movies[i].poster_path}`;
+            var backdropPath = `https://image.tmdb.org/t/p/original${movies[i].backdrop_path}`;
+            var movieTitle = movies[i].title;
+            var movieReleaseDate = movies[i].release_date;
+            var overview = movies[i].overview;
+
+            $('#moviePoster').attr('src', posterPath);
+
+            $('#movieTitle').html(movieTitle);
+            $('#releaseDate').html(movieReleaseDate);
+            $('#overview').html(overview);
+        }
+
 })
     .catch((error) => {
         console.error(error);
