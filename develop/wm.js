407b0b22f8d0278d9511adf8ffae7cbb9ae82b8d
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-02-27 10:28:51 -0400

    added beginning of watch later

--- develop/wm.js
+++ develop/wm.js
@@ -7,15 +7,42 @@ closeWatchLaterEl.addEventListener('click', watchLaterModalToggle);
 // Function to open and close modal
 function watchLaterModalToggle(){
   $(document).ready(function() {
-    $('#watchMoreMovies').toggle();
+    $('#watchLaterWrapper').toggle();
   });
 }
+
 watchLater = [];
+
 //Add to Watch Later Functionality
   const watchLaterButtonEl = document.getElementById('watchLaterBtn');
   watchLaterButtonEl.addEventListener('click', saveWatchLater);
+  const modalMovieTitleEl = document.getElementById("modalTitle");
+
+  // Adds new entry to local storage
   function saveWatchLater(){
- 
+    watchLater.push(modalMovieTitleEl.text());
+    localStorage.setItem('MyWatchLaterList', watchLater);
   }
+
+  // Creates the elements in the watch later modal
+  function newWatchLaterEntry(){
+  const watchLaterBodyEl = document.getElementById('watchLaterWrapper'); // replace with ID of search history display element
+  watchLaterBodyEl.innerHTML=''; // use innerHTML instead of textContent to clear previous contents
+  const watchLaterData = JSON.parse(localStorage.getItem('watchLaterList')) || []; // use empty array as default value if search history not found in local storage
+
+  /*
+  watchLaterData.forEach(searchTerm => {
+    const button = document.createElement('button'); // create button element
+    button.classList.add('history-button'); // add class to button for styling
+    button.textContent = searchTerm;
+    button.addEventListener('click', function() {
+      //performSearch(searchTerm); // replace with function that performs a search using the clicked search term
+    });
+    searchHistorySpace.appendChild(button);
+  });
+*/
+  }
+
+
 
 // Remove from Watch Later Functionality
