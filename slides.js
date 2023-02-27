9dfbb65d927a1fd3bd8cd68bc14884c0ab7632e8
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-02-27 14:21:47 -0400

    added button

--- slides.js
+++ slides.js
@@ -191,7 +191,7 @@ fetch(nowPlayingUrl)
 
 
               if (index === 0) {
-                var videoUrlrender = `<iframe class="responsive" width="1250" height="600" src="${Urltogo}" frameborder="0" allowfullscreen allow ="autoplay"; mute></iframe>`;
+                var videoUrlrender = `<iframe class="responsive" width="1200" height="600" src="${Urltogo}" frameborder="0" allowfullscreen allow ="autoplay"; mute></iframe>`;
               } else {
                 var videoUrlrender = `<iframe class="responsive" width="560" height="315" src="${Urltogo}" frameborder="0" allowfullscreen></iframe>`;
               }
@@ -223,14 +223,43 @@ fetch(nowPlayingUrl)
 
 
     
-      $(".Upcoming").on('click', function(){
-        console.log("i am clicking");
-        $(".slidesImagesrecent").hide();
-        $(".slidesImagesonair").hide()
-        $(".slidesImagesmain").hide()
-        $(".search ").hide();  
-        $(".slidesImagesvideo").show();
-        
-        
-        })
 
+        
+        
+document.addEventListener('DOMContentLoaded', () => {
+              // Functions to open and close a modal
+              function openModal($el) {
+                $el.classList.add('is-active');
+              }
+            
+              function closeModal($el) {
+                $el.classList.remove('is-active');
+              }
+            
+              function closeAllModals() {
+                (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+                  closeModal($modal);
+                });
+              }
+            
+              // Add a click event on buttons to open a specific modal
+              (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+                const modal = $trigger.dataset.target;
+                const $target = document.getElementById(modal);
+            
+                $trigger.addEventListener('click', () => {
+                  openModal($target);
+                  $(".slidesImagesvideo").show();
+                });
+              });
+            
+              // Add a click event on various child elements to close the parent modal
+              (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+                const $target = $close.closest('.modal');
+            
+                $close.addEventListener('click', () => {
+                  closeModal($target);
+                });
+              });
+            
+          });
