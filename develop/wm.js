b608daf444a6209e1646e75aaf1cff41fa2b3bdd
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-02-28 16:02:48 -0400

    still working on watch later

--- develop/wm.js
+++ develop/wm.js
@@ -18,20 +18,57 @@ watchLater = [];
   watchLaterButtonEl.addEventListener('click', saveWatchLater);
   const modalMovieTitleEl = document.getElementById("modalTitle");
 
-  // Adds new entry to local storage
+  // Adds new entry to local storage when the watch later button is clicked
   function saveWatchLater(){
-    watchLater.push(modalMovieTitleEl.text());
+    console.log("the watch later button el has been clicked");
+    watchLater.push(modalMovieTitleEl.textContent); // Fixed so that it now saves to local storage in an array!
     localStorage.setItem('MyWatchLaterList', watchLater);
   }
 
-  // Creates the elements in the watch later modal
-  function newWatchLaterEntry(){
-  const watchLaterBodyEl = document.getElementById('watchLaterWrapper'); // replace with ID of search history display element
-  watchLaterBodyEl.innerHTML=''; // use innerHTML instead of textContent to clear previous contents
-  const watchLaterData = JSON.parse(localStorage.getItem('watchLaterList')) || []; // use empty array as default value if search history not found in local storage
-  }
 
 // Remove from Watch Later Functionality
   function removeWatchLaterEntry(){
-    
+  
   }
+
+  const WatchLaterListEl = document.getElementById("watchLaterListWrapper");
+//Set Watch Later to be generated upon page loading //
+window.onload = function(){
+  WatchLaterListEl.innerHTML='';
+  const watchLaterData = JSON.parse(localStorage.getItem('watchLaterList')) || []; // use empty array as default value if search history not found in local storage
+// Loop through the array
+for (var i = 0; i < watchLaterData.length; i++) {
+    // Get the name of the current item
+    var itemName = watchLaterData[i].name;
+
+    // Create a new div element
+    var div = $('<div></div>');
+  
+    // Set the content of the div to the item name
+    div.text(itemName);
+  
+    // Append the div to the document body or another parent element
+    $('body').append(div);
+  }
+}
+
+// Set watch later to be updated every time a new entry is added //
+function generateNewWatchLaterContent(){
+  WatchLaterListEl.innerHTML='';
+  const watchLaterData = JSON.parse(localStorage.getItem('watchLaterList')) || []; // use empty array as default value if search history not found in local storage
+// Loop through the array
+for (var i = 0; i < watchLaterData.length; i++) {
+    // Get the name of the current item
+    var itemName = watchLaterData[i].name;
+
+    // Create a new div element
+    var div = $('<div></div>');
+  
+    // Set the content of the div to the item name
+    div.text(itemName);
+  
+    // Append the div to the document body or another parent element
+    $('body').append(div);
+  }
+}
+
